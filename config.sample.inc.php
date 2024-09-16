18f962cae2cf26bcaad7014af054f7cef1a0d67d
Ronny Wang <ronnywang@gmail.com>
2024-09-16 14:21:51 +0800

    update config.sample

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -1,3 +1,5 @@
 <?php
 
-putenv('APP_NAME', 'Mini Engine sample application');
+putenv('APP_NAME=Mini Engine sample application');
+putenv('DATABASE_URL=pgsql://user:password@localhost:5432/dbname');
+putenv('SESSION_SECRET=__random_string__');
