93aafdf55b883fb450dcd1233807d88f4f8363b3
Ronny Wang <ronnywang@gmail.com>
2024-09-18 08:45:42 +0800

    add meeting list

--- views/index/index.php
+++ views/index/index.php
@@ -3,6 +3,9 @@
 <p>Hello, <?= $this->escape($this->user->email) ?></p>
 Your meetings:
 <ul>
+    <?php foreach (Meeting::search(['owner_id' => $this->user->id]) as $meeting) { ?>
+    <li><a href="/meeting/show/<?= $meeting->uid ?>"><?= $this->escape($meeting->d('name')) ?></a></li>
+    <?php } ?>
     <li><a href="/meeting/create">Create a new meeting</a></li>
 </ul>
 <a href="/user/logout">Logout</a>
