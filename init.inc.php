e9325ff5cbb6521275ba3558bc55f1146d8c413a
Ronny Wang <ronnywang@gmail.com>
2024-09-20 12:40:21 +0800

    add production config

--- init.inc.php
+++ init.inc.php
@@ -5,6 +5,8 @@ define('MINI_ENGINE_ROOT', __DIR__);
 require_once(__DIR__ . '/mini-engine.php');
 if (file_exists(__DIR__ . '/config.inc.php')) {
     include(__DIR__ . '/config.inc.php');
+} elseif (file_exists("/srv/config/our-minutes.inc.php")) {
+    include("/srv/config/our-minutes.inc.php");
 }
 set_include_path(
     __DIR__ . '/libraries'
